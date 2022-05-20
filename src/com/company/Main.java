98ae012b139d8ea2278e3fa3f54ba5e317cd4986
Serek <jakubseredynski@gmail.com>
2022-05-20 22:56:04 +0200

    Cleaning repo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) throws EmptyStackException, FullStackException {
 	    RPNcalculator calc = new RPNcalculator();
 
-        System.out.println(calc.calculate("12 2 3 4 * 10 5 / + * +"));
+        String phrase = "2 2 +";
+
+        System.out.println(phrase);
+        System.out.println(calc.calculate(phrase));
     }
 }
